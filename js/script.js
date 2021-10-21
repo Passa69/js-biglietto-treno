0a7b98f68630f7b6a4aa25e2e4092c5a23e2ce1d
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-21 15:53:31 +0200

    richieste per l'utente

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // A seconda dei dati dell'utente il pc dovrà generare un prezzo per il biglietto
 
 // Chiediamo i chilometri
+let km = parseInt(prompt("Scrivi qui la distanza da percorrere: "));
+console.log(km);
 
 // Chiediamo l'età
+let age = parseInt(prompt("Scrivi qui la tua età: "));
+console.log(age);
 
 // Calcolo del prezzo
 
