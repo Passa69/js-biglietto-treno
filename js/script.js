0281ed88896609dd571101b75f565f42b45d07a6
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-21 16:28:50 +0200

    aggiunta degli sconti a seconda dell'età

--- js/script.js
+++ js/script.js
@@ -9,7 +9,20 @@ let age = parseInt(prompt("Scrivi qui la tua età: "));
 console.log(age);
 
 // Calcolo del prezzo
-let molt = km * 0.21; 
-console.log(molt);
+let ticket = km * 0.21; 
+console.log(ticket);
 
-// Stampa del prezzo
+let minor = (ticket / 100) * 20;
+console.log(minor);
+
+let over = (ticket / 100) * 40;
+console.log(over);
+
+// Aggiunta degli sconti
+if (age >= 18 && age <= 65) {
+    document.getElementById("mio_id").innerHTML = `Il tuo biglietto costa: ${ticket}€`
+} else if (age < 18) {
+    document.getElementById("mio_id").innerHTML = `Il tuo biglietto costa: ${minor}€`
+} else {
+    document.getElementById("mio_id").innerHTML = `Il tuo biglietto costa: ${over}€`
+}
